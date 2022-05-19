c5b44c177619a0f28de53a8f67579a843df7c507
Jared Dietz <jaredsurya@gmail.com>
2022-05-19 19:21:44 -0400

    sending first fetch to api, rendering artist title and 1 image. Catch null artist.

--- script.js
+++ script.js
@@ -12,3 +12,58 @@
 // mvid.php?i=(artistid}
 // Example - theaudiodb.com/api/v1/json/{APIKEY}/mvid.php?i=112024
 
+// BANDSINTOWN APP ID: 96950d65ce9ce5445e9876ef7a980447 (good for 3 months)
+
+const goBtn = document.getElementById("goBtn")
+const detailsBtn = document.getElementById("details")
+const discogBtn = document.getElementById("discog")
+const vidsBtn = document.getElementById("vids")
+const topBtn = document.getElementById("top")
+const inputForm = document.getElementById("formInput")
+const artistTitle = document.getElementById("name")
+const mainImg = document.getElementById("mainImg")
+const buttons = document.getElementsByClassName("buttons")
+// add event listener to GO! button and preventDefault
+goBtn.addEventListener('click', submitHandler)
+
+function submitHandler(event){
+  event.preventDefault()
+  console.log(inputForm.value)
+  artistFetch(inputForm.value)
+  //buttonEnable(inputForm.value)
+  inputForm.value = ""
+}
+
+function artistFetch(string){
+  let artistURL = `https://theaudiodb.com/api/v1/json/2/search.php?s=` + string
+  //console.log(artistURL)
+  fetch(artistURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    renderArtist(data)
+  })
+}
+
+function renderArtist(data){
+  if(data.artists === null){
+    console.log("NO ARTIST")
+    artistTitle.innerText = "Sorry, no artist found!"
+    mainImg.style.visibility = "hidden"
+    for (const button of buttons){
+      button.style.visibility = "hidden"
+    }
+  } else {
+    artistTitle.innerText = data.artists[0].strArtist
+    mainImg.src = data.artists[0].strArtistFanart
+    mainImg.style.visibility = "visible"
+    for(const button of buttons){
+      console.log("HERE")
+      button.style.visibility = 'visible'
+    }
+  }
+}
+
+function buttonEnable(input){
+
+}
