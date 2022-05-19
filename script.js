7915b6f66655a8328e227c524a64f862eb34fa66
Jared Dietz <jaredsurya@gmail.com>
2022-05-18 22:53:34 -0400

    organizing html header and search bar w/ basic css

---
+++ script.js
@@ -0,0 +1,14 @@
+// FROM: https://www.theaudiodb.com/api_guide.php?ref=apilist.fun
+
+// Return Artist details from artist name
+// search.php?s={Artist name}
+// Example - theaudiodb.com/api/v1/json/2/search.php?s=coldplay
+
+// Return Discography for an Artist with Album names and year only
+// discography.php?s={Artist_Name}
+// Example - theaudiodb.com/api/v1/json/{APIKEY}/discography.php?s=coldplay
+
+// Return all the Music videos for a known TADB_Artist_ID
+// mvid.php?i=(artistid}
+// Example - theaudiodb.com/api/v1/json/{APIKEY}/mvid.php?i=112024
+
