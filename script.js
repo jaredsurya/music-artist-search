d187c11e0f1f677fc899ada2c96226a79b7e09c8
Jared Dietz <jaredsurya@gmail.com>
2022-05-20 16:50:27 -0400

    got music videos rendering

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 
 // Return all the Music videos for a known TADB_Artist_ID
 // mvid.php?i=(artistid}
-// Example - theaudiodb.com/api/v1/json/{APIKEY}/mvid.php?i=112024
+// https://theaudiodb.com/api/v1/json/2/mvid.php?i=
 
 // BANDSINTOWN APP ID: 96950d65ce9ce5445e9876ef7a980447 (good for 3 months)
 
@@ -24,6 +24,9 @@ const inputForm = document.getElementById("formInput")
 const artistTitle = document.getElementById("name")
 const mainImg = document.getElementById("mainImg")
 const buttons = document.getElementsByClassName("buttons")
+let vidsArea = document.getElementById("artistVids")
+let detailsArea = document.getElementById("artistDetails")
+let discogArea = document.getElementById("artistDiscog")
 let artistData
 let discogData
 let mvData
@@ -33,8 +36,15 @@ goBtn.addEventListener('click', submitHandler)
 function submitHandler(event){
   event.preventDefault()
   //console.log(inputForm.value)
+  vidsArea.innerHTML = ""
+  detailsArea.innerHTML = ""
+  discogArea.innerHTML = ""
+  if (inputForm.value == ""){
+    alert("Type in an artist to query first.")
+  } else
   artistFetch(inputForm.value)
   //buttonEnable(inputForm.value)
+
   inputForm.value = ""
 }
 
@@ -44,7 +54,7 @@ function artistFetch(string){
   fetch(artistURL)
   .then((response) => response.json())
   .then((data) => {
-    //console.log(data)
+    console.log(data)
     renderArtist(data)
     buttonEnable()
 
@@ -85,8 +95,8 @@ function activate(e) {
         //console.log("DISCOG CLICKED")
         fetchDiscog(artistData)        
       } else if (e.target.id === "vids"){
-        console.log("VIDS CLICKED")
-        
+        //fconsole.log("VIDS CLICKED")
+        renderVids(artistData)
       } else if (e.target.id === "top"){
         console.log("TOP CLICKED")
         window.scrollTo(0, 0)
@@ -98,17 +108,44 @@ function renderDetails(artistData){
 }
 
 function fetchDiscog(artistData){
-  let artistName = artistData.strArtist
-  fetch("https://theaudiodb.com/api/v1/json/2/discography.php?s=" + artistName)
+  let artistID = artistData.idArtist
+  fetch("https://theaudiodb.com/api/v1/json/2/album.php?i=" + artistID)
   .then(response => response.json())
   .then((data) => renderDiscog(data))
 
 }
 
 function renderDiscog(data){
-  console.log(data)
+  let fullDiscog = data.album
+  console.log(fullDiscog)
+  for (const album in fullDiscog){
+
+  }
+
 }
 
 function renderVids(data){
-
+  //console.log(data)
+  fetch("https://theaudiodb.com/api/v1/json/2/mvid.php?i=" + data.idArtist)
+  .then(res => res.json())
+  .then(vidData => listVids(vidData))
+  function listVids(vidData){
+    vidData.mvids.forEach((vid) => {
+      console.log(vid)
+      let container = document.createElement("div")
+      container.className = "mVids"
+      let track = document.createElement("h3")
+      track.className = "title"
+      track.innerText = vid.strTrack
+      track.href = vid.strMusicVid
+      let trackLink = document.createElement("a")
+      trackLink.className = "link"
+      trackLink.href = vid.strMusicVid
+      trackLink.innerText = "Click here to watch."
+      let trackDescription = document.createElement("p")
+      trackDescription.innerText = vid.strDescriptionEN
+      container.append(track, trackLink, trackDescription)
+      vidsArea.append(container)
+    })
+  }
 }
